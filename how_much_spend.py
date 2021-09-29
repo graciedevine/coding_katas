1ad561876d2944c19fc0f840e79eda13130b63b1
Gracie <68759643+graciedevine@users.noreply.github.com>
2021-09-29 08:43:26 -0400

    cleaned up function parameters

--- how_much_spend.py
+++ how_much_spend.py
@@ -8,15 +8,16 @@
 # -> 5+60 = 65
 # -> 65 + 0.09 of 65 = 70.85
 # -> Output: 70.85
-costs = {"socks": 5, "shoes": 60, "sweater": 30}
 
 
 def get_total(costs, items, tax):
     gross_cost = 0
     for item in items:
         if item in costs.keys():
-            gross_cost += costs[item] # items in a dictionary are accessed using square brackets
+            gross_cost += costs[
+                item
+            ]  # items in a dictionary are accessed using square brackets
     return round(gross_cost + tax * gross_cost, 2)
 
 
-print(get_total(costs, ["socks", "shoes"], 0.09))
+print(get_total({"socks": 5, "shoes": 60, "sweater": 30}, ["socks", "shoes"], 0.09))
