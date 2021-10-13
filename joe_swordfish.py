3857f1fc2bf34b65303b92a7966114bf3b5f41ed
Gracie <68759643+graciedevine@users.noreply.github.com>
2021-10-13 12:00:17 -0400

    Delete joe_swordfish.py

--- joe_swordfish.py
+++
@@ -1,9 +0,0 @@
-while True:
-  name = input('Who are you?')
-  if name != 'Joe':
-    continue
- 
-  password = input('Hello, Joe. What is the password? (It is a fish.)')
-  if password == 'swordfish':
-    break
-print('Access granted.')
