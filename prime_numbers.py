66c96824ac9bc471e704ee4f20f7128d744b0d9a
Gracie <68759643+graciedevine@users.noreply.github.com>
2021-11-04 10:55:45 -0400

    completed

--- prime_numbers.py
+++ prime_numbers.py
@@ -4,12 +4,11 @@ import math
 def print_primes(n):
     for x in range(1, n + 1):
         prime = True
-        for y in range(2, int(math.sqrt(n)) + 1):
+        for y in range(2, x):
             if x % y == 0:
                 prime = False
-            if prime:
-                print(x)
-                break
+        if prime:
+            print(x)
 
 
 print_primes(15)
